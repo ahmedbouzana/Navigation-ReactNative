2696505b61775c4675e4138a60a71442b6bb191b
ahmedbouzana <ah.bouzana@gmail.com>
2019-04-23 20:18:14 +0200

    Add createAppContainer

--- App.js
+++ App.js
@@ -6,16 +6,19 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
-import {createStackNavigator } from "react-navigation"
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>This is App component!</Text>
-        <Button onPress={()=> this.props.navigation.navigate('test')} title="Go to Test" ></Button>
+        <Button
+          onPress={() => this.props.navigation.navigate("test")}
+          title="Go to Test"
+        />
       </View>
     );
   }
@@ -26,7 +29,10 @@ class Test extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>This is Test component!</Text>
-        <Button onPress={()=> this.props.navigation.navigate('home')} title="Go to App" ></Button>
+        <Button
+          onPress={() => this.props.navigation.navigate("home")}
+          title="Go to App"
+        />
       </View>
     );
   }
@@ -35,23 +41,25 @@ class Test extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
 
-export default createStackNavigator ({
-home:App,
-test:Test
-})
+const RootStack = createStackNavigator({
+  home: App,
+  test: Test
+});
+
+export default createAppContainer(RootStack);
